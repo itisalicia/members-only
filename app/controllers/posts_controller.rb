2868716b0441d2c946185a7e9200c28148975326
Alicia <alicia.magro@ouidou.fr>
2025-06-02 16:40:58 +0200

    Create a form to create a new post, and it's working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
 
-before_action :authenticate_user!, only [ :new, :create ]
+before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @posts = Post.all 
@@ -14,7 +14,7 @@ before_action :authenticate_user!, only [ :new, :create ]
     @post = Post.new(post_params)
     @post.user = current_user # Immediately associate the post with the current user
     if @post.save
-      redirect_to root_path, notice "Your post was created successfully."
+      redirect_to root_path, notice: "Your post was created successfully."
     else
       render :new, status: :unprocessable_entity
     end
